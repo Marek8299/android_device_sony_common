7bbcdedd55dfd89b67657ef2b2b31012e4d90ca8
Marek8299 <marek8299@outlook.com>
2024-08-07 19:31:22 +0200

    do not force build system to build toybox

--- toybox/Android.mk
+++ toybox/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := toybox-to-utilities
-LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE := toybox-init
+LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_MODULE_FILE := $(PRODUCT_OUT)/system/bin/toybox-static32
+LOCAL_SRC_FILES := toybox-static
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
-LOCAL_INSTALLED_MODULE_STEM := toybox-static
 
 include $(BUILD_PREBUILT)
+
